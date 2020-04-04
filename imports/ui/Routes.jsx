9f7b82d536c015f6ecc0b76270d2c3e171c36d19
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-04 18:53:45 -0300

    fix: adjust register form

--- imports/ui/Routes.jsx
+++ imports/ui/Routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Welcome from "./pages/Welcome";
+import { Register } from 'meteor/cereal:ui/pages/Register';
 
 export default () => {
   return(
     <Switch>
-      <Route path={"/"}>
+      <Route exact path={"/"}>
         <Welcome />
       </Route>
     </Switch>
